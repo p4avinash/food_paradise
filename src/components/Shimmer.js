3eb94417e106fe88393cd3caf4c8ef704f64bef2
p4avinash <p4avinashkumar@gmail.com>
2024-01-09 12:45:54 +0530

    minor change

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -81,15 +81,6 @@ const Shimmer = () => {
           <p></p>
         </div>
       </div>
-      <div className='shimmer-card'>
-        <div className='shimmer-img' />
-        <p className='shimmer-name'></p>
-        <p className='shimmer-cuisines'></p>
-        <div className='shimmer-other-details'>
-          <p></p>
-          <p></p>
-        </div>
-      </div>
     </div>
   )
 }
